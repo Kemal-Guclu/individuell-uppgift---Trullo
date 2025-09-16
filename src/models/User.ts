ff99ae2fcb9ffc64d795acc72224690ac23f85b1
Kemal-Guclu <kemal.guclu@chasacademy.se>
2025-09-16 17:13:01 +0200

    added User-modell

---
+++ src/models/User.ts
@@ -0,0 +1,23 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    passwordHash: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+// userSchema.index({ email: 1 }, { unique: true });
+
+export const User = model("User", userSchema);
